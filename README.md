d461b395ae1d7c57eb467d5646fa541ae6fd23e4
Ryan Schmidt <skizzerz@skizzerz.net>
2021-11-11 09:54:57 -0700

    Add button for the central account management to Special:Preferences

--- README.md
+++ README.md
@@ -17,6 +17,13 @@ This extension configures MediaWiki to accept Keycloak authentication headers as
 
 ## Configuration
 
-None. The extension will automatically configure Auth_remoteuser.
+The extension will automatically configure Auth_remoteuser.
 Any changes made by the user to Auth_remoteuser configuration may cause the extension
 to not work as anticipated.
+
+### $wgKeycloakAuthPortalUrl
+*Default:* `''`
+
+If set, the extension will add a button to Special:Preferences leading the user to this URL.
+This should be set to the Keycloak portal where they can view and update their email and password.
+If left as the default (an empty string), no button will be added to Special:Preferences.
--- src/Hooks.php
+++ src/Hooks.php
@@ -4,9 +4,13 @@ namespace KeycloakAuth;
 
 use DatabaseUpdater;
 use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
+use MediaWiki\MediaWikiServices;
+use MediaWiki\Preferences\Hook\GetPreferencesHook;
+use OOUI\ButtonWidget;
 use RuntimeException;
+use User;
 
-class Hooks implements LoadExtensionSchemaUpdatesHook {
+class Hooks implements GetPreferencesHook, LoadExtensionSchemaUpdatesHook {
 	/**
 	 * Create our keycloak_user table.
 	 *
@@ -32,5 +36,36 @@ class Hooks implements LoadExtensionSchemaUpdatesHook {
 			'keycloak_user',
 			dirname( __FILE__, 2 ) . "/sql/$type/keycloak_user.sql"
 		);
+	}
+
+	/**
+	 * Add a button leading to the shared account management portal, if specified.
+	 *
+	 * @param User $user User whose preferences are being modified
+	 * @param array &$preferences Preferences description array, to be fed to an HTMLForm object
+	 * @return void To continue hook execution
+	 *
+	 */
+	public function onGetPreferences( $user, &$preferences ) {
+		$config = MediaWikiServices::getInstance()->getMainConfig();
+		$portalUrl = $config->get( 'KeycloakAuthPortalUrl' );
+		if ( $portalUrl === '' ) {
+			// not configured
+			return;
+		}
+
+		$button = new ButtonWidget( [
+			'href' => $portalUrl,
+			'label' => 'keycloakauth-prefs-button'
+		] );
+
+		$preferences['keycloakauth'] = [
+			'section' => 'personal/info',
+			'type' => 'info',
+			'raw' => true,
+			'label-message' => 'keycloakauth-prefs-label',
+			'help-message' => 'keycloakauth-prefs-help',
+			'default' => (string)$button
+		];
 	}
 }
